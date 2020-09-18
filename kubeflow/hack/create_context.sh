e737f7d12a79b3062e2cbefb0b920afbd2aff4a3
subodh101 <subodh.pushkar@gmail.com>
2020-09-18 20:46:24 +0900

    update comments and config deletion output based on review

--- kubeflow/hack/create_context.sh
+++ kubeflow/hack/create_context.sh
@@ -8,8 +8,9 @@
 # TODO(jlewi): Support zonal clusters as well
 set -x
 
-# Delete kubeconfig context if exist already
-kubectl config delete-context ${NAME} || echo "Ignore error - cannot delete context ${NAME}"
+# Delete the existing kubeconfig, because it may be an outdated
+# context to a deleted cluster with the same name.
+kubectl config delete-context ${NAME} || echo "Context ${NAME} doesn't exist, Rename the context step will create it"
 
 set -ex
 
