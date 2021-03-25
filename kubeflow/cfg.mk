f6bea590185b154e51b75a2952c6d4ffb893d162
James Liu <jamxl@google.com>
2021-03-25 23:01:41 +0000

    use packages for gcp_config instead of upstream/manifests/distributions/gcp

--- kubeflow/cfg.mk
+++ kubeflow/cfg.mk
@@ -7,15 +7,12 @@ set-cfg-values:
 
 	kpt cfg set ./instance mgmt-ctxt <YOUR_MANAGEMENT_CTXT>
 
-	kpt cfg set ./upstream/manifests/distributions/gcp name <YOUR_KF_NAME>
-	kpt cfg set ./upstream/manifests/distributions/gcp gcloud.core.project <PROJECT_TO_DEPLOY_IN>
-	kpt cfg set ./upstream/manifests/distributions/gcp gcloud.compute.zone <ZONE>
-	kpt cfg set ./upstream/manifests/distributions/gcp location <REGION OR ZONE>
-	kpt cfg set ./upstream/manifests/distributions/gcp log-firewalls false
-
-	kpt cfg set ./upstream/manifests/distributions/stacks/gcp name <YOUR_KF_NAME>
-	kpt cfg set ./upstream/manifests/distributions/stacks/gcp gcloud.core.project <PROJECT_TO_DEPLOY_IN>
-
+	kpt cfg set ../packages/gcp-resources name <YOUR_KF_NAME>
+	kpt cfg set ../packages/gcp-resources  gcloud.core.project <PROJECT_TO_DEPLOY_IN>
+	kpt cfg set ../packages/gcp-resources  gcloud.compute.zone <ZONE>
+	kpt cfg set ../packages/gcp-resources  location <REGION OR ZONE>
+	kpt cfg set ../packages/gcp-resources  log-firewalls false
+	
 	kpt cfg set ./instance name <YOUR_KF_NAME>
 	kpt cfg set ./instance location <YOUR_REGION or ZONE>
 	kpt cfg set ./instance gcloud.core.project <YOUR PROJECT>
