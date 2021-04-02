af2d23c3539355a7b7707f60e965b2379dbb57d8
James Liu <jamxl@google.com>
2021-04-02 22:21:51 +0000

    pull manifest v1.3.0-rc.0

--- pull_upstream.sh
+++ pull_upstream.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-export KUBEFLOW_MANIFESTS_VERSION=master
+export KUBEFLOW_MANIFESTS_VERSION=v1.3.0-rc.0
 # export KUBEFLOW_MANIFESTS_VERSION=v1.3.0
 # export KUBEFLOW_MANIFESTS_VERSION=v1.2.0
 export KUBEFLOW_MANIFESTS_REPO=https://github.com/kubeflow/manifests.git
