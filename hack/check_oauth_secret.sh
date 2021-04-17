65c3510299872e6adc0624d888b2965ccde0ab65
James Liu <jamxl@google.com>
2021-04-17 05:14:42 +0000

    (cleanup) Remove redundant files

--- hack/check_oauth_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Check if CLIENT_ID and CLIENT_SECRET are set
-
-if [ -z "$CLIENT_ID" ] || [ -z "$CLIENT_SECRET" ]; then
-   echo "Error: Environment variables CLIENT_ID and CLIENT_SECRET must be set to the OAuth client id and secret to be used with IAP"
-   exit 1
-fi	
