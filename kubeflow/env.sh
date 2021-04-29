d1aed6278b94fec0e7e21ddf22c32e0ea11802ef
James Liu <jamxl@google.com>
2021-04-29 19:33:40 +0000

    address comments

--- kubeflow/env.sh
+++ kubeflow/env.sh
@@ -1,8 +1,10 @@
 export KF_NAME=<kubeflow-cluster-name>
 export KF_PROJECT=<gcp-project-id>
+export KF_PROJECT_NUMBER=<google-cloud-project-number>
 export KF_DIR=<kubeflow-download-path>
 export MGMT_NAME=<management-cluster-name>
 export MGMTCTXT=${MGMT_NAME}
 export LOCATION=<zone>
+export ADMIN_EMAIL=<administrator-email-address>
 
 source ./common/asm/env.sh
--- kubeflow/kpt-set.sh
+++ kubeflow/kpt-set.sh
@@ -8,10 +8,10 @@ kpt cfg set -R .  gke.private false
 kpt cfg set -R .  mgmt-ctxt ${MGMT_NAME}
 
 kpt cfg set -R .  name ${KF_NAME}
-kpt cfg set -R .  gcloud.project.projectNumber <KF_PROJECT_NUMBER>
+kpt cfg set -R .  gcloud.project.projectNumber ${KF_PROJECT_NUMBER}
 kpt cfg set -R .  gcloud.core.project ${KF_PROJECT}
 kpt cfg set -R .  gcloud.compute.zone ${LOCATION}
 kpt cfg set -R .  location ${LOCATION}
 kpt cfg set -R .  log-firewalls false
 
-kpt cfg set -R .  email <YOUR_EMAIL_ADDRESS>
+kpt cfg set -R .  email ADMIN_EMAIL
