2bdc46d9932912c2a04796e9411d22eb6e409a93
James Liu <jamxl@google.com>
2021-05-03 18:06:04 +0000

    (env.sh) Move all envrionment variable config to env.sh

--- kubeflow/kpt-set.sh
+++ kubeflow/kpt-set.sh
@@ -7,10 +7,10 @@ kpt cfg set -R .  asm-label ${ASM_LABEL}
 kpt cfg set -R .  mgmt-ctxt ${MGMT_NAME}
 
 kpt cfg set -R .  name ${KF_NAME}
-kpt cfg set -R .  gcloud.project.projectNumber <KF_PROJECT_NUMBER>
+kpt cfg set -R .  gcloud.project.projectNumber ${KF_PROJECT_NUMBER}
 kpt cfg set -R .  gcloud.core.project ${KF_PROJECT}
 kpt cfg set -R .  gcloud.compute.zone ${LOCATION}
 kpt cfg set -R .  location ${LOCATION}
 kpt cfg set -R .  log-firewalls false
 
-kpt cfg set -R .  email <YOUR_EMAIL_ADDRESS>
+kpt cfg set -R .  email ${ADMIN_EMAIL}
