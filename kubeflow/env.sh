5f53bfb7845e75b2b80eb2584167b04a34337f70
James Liu <jamxl@google.com>
2021-05-05 17:50:51 +0000

    patch the quote around variables

--- kubeflow/env.sh
+++ kubeflow/env.sh
@@ -1,9 +1,9 @@
 export KF_NAME=<kubeflow-cluster-name>
 export KF_PROJECT=<gcp-project-id>
-export KF_PROJECT_NUMBER=$(gcloud projects describe ${KF_PROJECT} --format='value(projectNumber)')
+export KF_PROJECT_NUMBER=$(gcloud projects describe "${KF_PROJECT}" --format='value(projectNumber)')
 export KF_DIR=<kubeflow-download-path>
 export MGMT_NAME=<management-cluster-name>
-export MGMTCTXT=${MGMT_NAME}
+export MGMTCTXT="${MGMT_NAME}"
 export LOCATION=<zone>
 export ADMIN_EMAIL=<administrator-full-email-address>
 
--- kubeflow/kpt-set.sh
+++ kubeflow/kpt-set.sh
@@ -5,13 +5,13 @@ bash common/asm/kpt-set.sh
 
 kpt cfg set -R .  gke.private false
 
-kpt cfg set -R .  mgmt-ctxt ${MGMT_NAME}
+kpt cfg set -R .  mgmt-ctxt "${MGMT_NAME}"
 
-kpt cfg set -R .  name ${KF_NAME}
-kpt cfg set -R .  gcloud.project.projectNumber ${KF_PROJECT_NUMBER}
-kpt cfg set -R .  gcloud.core.project ${KF_PROJECT}
-kpt cfg set -R .  gcloud.compute.zone ${LOCATION}
-kpt cfg set -R .  location ${LOCATION}
+kpt cfg set -R .  name "${KF_NAME}"
+kpt cfg set -R .  gcloud.project.projectNumber "${KF_PROJECT_NUMBER}"
+kpt cfg set -R .  gcloud.core.project "${KF_PROJECT}"
+kpt cfg set -R .  gcloud.compute.zone "${LOCATION}"
+kpt cfg set -R .  location "${LOCATION}"
 kpt cfg set -R .  log-firewalls false
 
-kpt cfg set -R .  email ${ADMIN_EMAIL}
+kpt cfg set -R .  email "${ADMIN_EMAIL}"
