d983318a372d7dc354658f65637a7e14207c81a1
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-05-14 00:13:27 +0800

    feat: refactor KFP managed storage a separate package. Fixes #275 (#272)
    * feat: refactor KFP managed storage a separate package
    
    * chore: reset values
    
    * chore: clean up root Makefile
    
    * fix management name setter default values
    
    * address comments
    
    * update env.sh kpt-set.sh README.md
    
    * chore: fix
    
    * update readme
    
    * update
    
    * chore: enable force destroy of bucket

--- kubeflow/env.sh
+++ kubeflow/env.sh
@@ -1,11 +1,55 @@
-export KF_NAME=<kubeflow-cluster-name>
-export KF_PROJECT=<gcp-project-id>
-export KF_DIR=<kubeflow-download-path>
-export MGMT_NAME=<management-cluster-name>
-export MGMTCTXT="${MGMT_NAME}"
-export LOCATION=<zone>
+# 1. Edit <placeholders>.
+# 2. Other env vars are configurable, but with default values set below.
+
+# The KF_PROJECT env var contains the Google Cloud project ID where Kubeflow
+# cluster will be deployed to.
+export KF_PROJECT=<google-cloud-project-id>
+# You can get your project number by running this command
+# (replace ${KF_PROJECT} with the actual project ID):
+# gcloud projects describe --format='value(projectNumber)' "${KF_PROJECT}"
+export KF_PROJECT_NUMBER=<google-cloud-project-number>
+# ADMIN_EMAIL env var is the Kubeflow admin's email address, it should be
+# consistent with login email on Google Cloud.
+# Example: admin@gmail.com
 export ADMIN_EMAIL=<administrator-full-email-address>
+# The MGMT_NAME env var contains the name of your management cluster created in
+# management cluster setup:
+# https://www.kubeflow.org/docs/distributions/gke/deploy/management-setup/
+export MGMT_NAME=<management-cluster-name>
+# The MGMTCTXT env var contains a kubectl context that connects to the management
+# cluster. By default, management cluster setup creates a context named
+# ${MGMT_NAME} for you.
+export MGMTCTXT="${MGMT_NAME}"
 
+######################
+# NOTICE: The following env vars have default values, but they are also configurable.
+######################
+
+# KF_NAME env var is name of your new Kubeflow cluster.
+# It should satisfy the following prerequisites:
+# * be unique within your project, e.g. if you already deployed cluster with the
+# name "kubeflow", use a different name when deploying another Kubeflow cluster.
+# * start with a lowercase letter
+# * only contain lowercase letters, numbers and "-"s (hyphens)
+# * end with a number or a letter
+# * contain no more than 24 characters
+export KF_NAME=kubeflow
+# Default values for managed storage used by Kubeflow Pipelines (KFP), you can
+# override as you like.
+# The CloudSQL instance and Cloud Storage bucket instance are created during
+# deployment, so you should make sure their names are not used before.
+export CLOUDSQL_NAME="${KF_NAME}-kfp"
+# Note, Cloud Storage bucket name needs to be globally unique across projects.
+# So we default to a name related to ${KF_PROJECT}.
+export BUCKET_NAME="${KF_PROJECT}-kfp"
+# LOCATION can either be a zone or a region, that determines whether the deployed
+# Kubeflow cluster is a zonal cluster or a regional cluster.
+# Specify LOCATION as a region like the following line to create a regional Kubeflow cluster.
+# export LOCATION=us-central1
+export LOCATION=us-central1-c
+# REGION should match LOCATION.
+export REGION=us-central1
+# Preferred zone of Cloud SQL. Note, ZONE should be in REGION.
+export ZONE=us-central1-c
+# Anthos Service Mesh version label
 export ASM_LABEL=asm-192-1
-
-export KF_PROJECT_NUMBER=$(gcloud projects describe ${KF_PROJECT} --format='value(projectNumber)')
--- kubeflow/hack/delete_gcp.sh
+++ kubeflow/hack/delete_gcp.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-set -e
+set -ex
 
-echo "Deleting all GCP resources will cause destruction of all services and data on this cluster. Confirm? [y/N]"; 
-read REPLY; 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then 
-    kubectl --context=$MGMTCTXT delete -f common/cnrm/$BUILD_DIR
+if [[ -z "${MGMTCTXT}" ]]; then
+    echo "MGMTCTXT env var is required"
+    exit 1
+fi
+
+echo "Deleting all GCP resources will cause destruction of all services and data on this cluster. Confirm? [y/N]";
+read REPLY;
+if [[ "${REPLY}" =~ ^[Yy]$ ]]
+then
+    BUILD_DIR="${BUILD_DIR:-build}"
+    kubectl --context="${MGMTCTXT}" delete -f "common/cnrm/${BUILD_DIR}"
 fi
