e46356f5260fbcbf17d3cd710be5ebffe636ff93
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-05-15 01:26:28 +0800

    chore: clean up doc + make Makefiles usable individually (#279)
    * docs: introduce component order and working with individual components
    
    * chore: make all makefiles usable individually
    
    * remove unused project number setter
    
    * remove unused Kptfile setters/substitutions
    
    * restore projectNumber setter
    
    * fix
    
    * Update Makefile
    
    * fix iap-ingress Kptfile

--- kubeflow/hack/apply_component.sh
+++ kubeflow/hack/apply_component.sh
@@ -3,12 +3,12 @@ set -e
 
 # Call certain component's make apply if Makefile exists,
 # or use kustomize and kubectl to apply the kustomization.yaml resources.
-# 
+#
 # Run this script under kubeflow/ path. (One level above current hack/ directory)
-# Run this as PROJECT=$(PROJECT) NAME=$(NAME) KFCTXT=$(KFCTXT) LOCATION=$(LOCATION) ./apply_component -path <component path under kubeflow>
-# For example: PROJECT=gcp-project NAME=cluster-name KFCTXT=cluster-context LOCATION=us-west1-b ./apply_component -path common/kubeflow-namespace
+# Run this as KFCTXT=$(KFCTXT) ./apply_component -path <component path under kubeflow>
+# For example: KFCTXT=cluster-context ./apply_component -path common/kubeflow-namespace
 # To set build directory other than ./build, use the following command:
-# BUILD_DIR=$(GENERATED_RESOURCE_LOCATION) PROJECT=$(PROJECT) NAME=$(NAME) KFCTXT=$(KFCTXT) LOCATION=$(LOCATION) ./apply_component -path <component path under kubeflow>
+# BUILD_DIR=$(GENERATED_RESOURCE_LOCATION) KFCTXT=$(KFCTXT) ./apply_component -path <component path under kubeflow>
 
 HYDRATE_ONLY="${HYDRATE_ONLY:=0}"
 COMPONENT_PATH=""
@@ -21,20 +21,20 @@ main() {
 
   COMPONENT_PATH=$2
   echo "Component path: $COMPONENT_PATH"
-  makefile_path=./$COMPONENT_PATH/Makefile
+  makefile_path="./$COMPONENT_PATH/Makefile"
   echo "Apply component resources: $COMPONENT_PATH"
-  if [ -f $makefile_path ] ; then 
+  if [ -f $makefile_path ] ; then
       echo "Found Makefile, call 'make apply' of this component Makefile. "
       if [ $HYDRATE_ONLY -ne 1 ]; then
-        NAME=$NAME KFCTXT=$KFCTXT LOCATION=$LOCATION PROJECT=$PROJECT make -C $COMPONENT_PATH apply build_dir=$BUILD_DIR
+        make -C $COMPONENT_PATH apply build_dir=$BUILD_DIR
       else
-        NAME=$NAME KFCTXT=$KFCTXT LOCATION=$LOCATION PROJECT=$PROJECT make -C $COMPONENT_PATH hydrate build_dir=$BUILD_DIR
+        make -C $COMPONENT_PATH hydrate build_dir=$BUILD_DIR
       fi
   else
       echo 'Makefile not found, use kustomize and kubectl to apply resources.'
-      component_build_dir=./$COMPONENT_PATH/$BUILD_DIR
-      rm -rf $component_build_dir && mkdir -p $component_build_dir
-      kustomize build -o $component_build_dir ./$COMPONENT_PATH
+      component_build_dir="./$COMPONENT_PATH/$BUILD_DIR"
+      rm -rf "$component_build_dir" && mkdir -p "$component_build_dir"
+      kustomize build -o "$component_build_dir" "./$COMPONENT_PATH"
       if [ $HYDRATE_ONLY -ne 1 ]; then
         kubectl --context=$KFCTXT apply -f $component_build_dir
       fi
