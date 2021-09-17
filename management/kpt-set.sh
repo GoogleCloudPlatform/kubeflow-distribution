03af776a9313587823469fa9cd894a38716db158
James Liu <37026441+zijianjoy@users.noreply.github.com>
2021-09-16 22:44:43 -0700

    kpt: Upgrade to kpt 1.0.0 for kubeflow/ and management/. Partial #308 (#311)
    * kpt: Upgrade to kpt 1.0.0 for kubeflow/
    
    * management migration

--- management/kpt-set.sh
+++ management/kpt-set.sh
@@ -1,3 +1,12 @@
-kpt cfg set -R . name "${MGMT_NAME}"
-kpt cfg set -R . gcloud.core.project "${MGMT_PROJECT}"
-kpt cfg set -R . location "${LOCATION}"
+# Deprecated for kpt pre-1.0.0 syntax.
+# kpt cfg set -R . name "${MGMT_NAME}"
+# kpt cfg set -R . gcloud.core.project "${MGMT_PROJECT}"
+# kpt cfg set -R . location "${LOCATION}"
+
+# kpt 1.0.0+ syntax
+kpt fn eval --image gcr.io/kpt-fn/apply-setters:v0.1 ./kptconfig -- \
+    name="${MGMT_NAME}" \
+    gcloud.core.project="${MGMT_PROJECT}" \
+    location="${LOCATION}" \
+
+kpt fn eval --image gcr.io/kpt-fn/apply-setters:v0.1 ./manifests --fn-config ./kptconfig/kpt-setter-config.yaml --truncate-output=false
