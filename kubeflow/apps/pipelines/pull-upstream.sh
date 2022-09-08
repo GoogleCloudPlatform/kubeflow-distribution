6cf41ff1a03de190b4fc831d6fb41b6396387694
Ablai Akhazhanov <gkcalat@ucla.edu>
2022-09-08 17:56:27 +0000

    Add comments to the changes, fix scope of env variables

--- kubeflow/apps/pipelines/pull-upstream.sh
+++ kubeflow/apps/pipelines/pull-upstream.sh
@@ -13,8 +13,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
+# Pulls manifests for Kubeflow Pipelines into the ./upstream folder
+# Arguments:
+#   $1 must be a Git repository. E.g. https://github.com/kubeflow/pipelines.git
+#   $2 must be a TAG or Commit SHA sum. E.g. 2.0.0-alpha.4
+# Output:
+#   Print messages for debugging
 set -ex
+
+if [ $# -eq 0 ]
+  then
+    echo "Provide input values for KUBEFLOW_PIPELINES_REPO and KUBEFLOW_PIPELINES_VERSION.
+    E.g. ./apps/pipelines/pull_upstream.sh https://github.com/kubeflow/pipelines.git 2.0.0-alpha.4"
+    exit 1
+fi
 
 # TODO: Use kubeflow/pipelines once https://github.com/kubeflow/pipelines/pull/6595 is resolved.
 export KUBEFLOW_PIPELINES_VERSION=$2
