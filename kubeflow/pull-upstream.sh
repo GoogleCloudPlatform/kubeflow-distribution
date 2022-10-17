1641d19ab254f2f0f02b5f51dbb201bdaab559f7
gkcalat <35157096+gkcalat@users.noreply.github.com>
2022-10-17 11:29:43 -0700

    Releasing GCP Kubeflow distribution v1.6.1 (#394)
    * Add shell in asm makefile. Fixes #389.
    
    * Upgrade MySQL to 8.0
    
    * Upgrade kfp to v2.0.0-alpha.5
    
    * Update CHANGELOG
    
    * Upgrade upstream to v1.6.1
    
    * Add namespace spcification
    
    * Update changelog and add a comment about CloudSQL
    
    * Increase delay before deleting iap helper workloads to 90 sec
    
    * Upgrade KFP to 2.0.0-alpha.6
    
    * Update changelog
    
    * Remove mysql version from comments
    
    * Fix asmcli installation path
    
    * Fix asmcli installation path
    
    * Fix upstream folder in ASM
    
    * Update upstream folder for asm

--- kubeflow/pull-upstream.sh
+++ kubeflow/pull-upstream.sh
@@ -16,9 +16,9 @@
 
 set -ex
 
-export KUBEFLOW_MANIFESTS_VERSION=v1.6.0
+export KUBEFLOW_MANIFESTS_VERSION=v1.6.1
 export KUBEFLOW_MANIFESTS_REPO=https://github.com/kubeflow/manifests.git
-export KUBEFLOW_PIPELINES_VERSION=2.0.0-alpha.4
+export KUBEFLOW_PIPELINES_VERSION=2.0.0-alpha.6
 export KUBEFLOW_PIPELINES_REPO=https://github.com/kubeflow/pipelines.git
 
 # Pull Kubeflow Pipelines upstream manifests.
