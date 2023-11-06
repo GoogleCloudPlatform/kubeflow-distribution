f08f9ca95d691f6d41c7840be4300c1e55d84b93
Chen Sun <chensun@users.noreply.github.com>
2023-11-06 15:18:37 -0800

    Update pull-upstream.sh

--- kubeflow/pull-upstream.sh
+++ kubeflow/pull-upstream.sh
@@ -16,7 +16,7 @@
 
 set -ex
 
-export KUBEFLOW_MANIFESTS_VERSION=v1.8
+export KUBEFLOW_MANIFESTS_VERSION=v1.8.0
 export KUBEFLOW_MANIFESTS_REPO=https://github.com/kubeflow/manifests.git
 export KUBEFLOW_PIPELINES_VERSION=2.0.3
 export KUBEFLOW_PIPELINES_REPO=https://github.com/kubeflow/pipelines.git
